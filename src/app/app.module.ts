a2399840f4f0a68f2a247962480c7bf00157e8ed
juhopes <juhopessi1@gmail.com>
2018-01-29 15:47:34 +0200

    monday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MediaService } from './services/media.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MediaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
